feff650d768a6382c0171b45c7dcf472898abfe9
gia-grigalashvili <grigalagia07@gmail.com>
2024-06-08 00:14:05 +0400

    import Typesforplanet.tsx in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css";
 import Navigation from "./Components/Navigation";
-
+import Typesforplanet from "./Components/Typesforplanet";
 function App() {
   return (
     <>
       <Navigation />
+      <Typesforplanet />
     </>
   );
 }
--- src/Components/Typesforplanet.tsx
+++ src/Components/Typesforplanet.tsx
@@ -1,6 +1,14 @@
 import React from "react";
+import data from "../data.json";
 
 function Typesforplanet() {
-  return <div>Typesforplanet</div>;
+  return (
+    <div>
+      {data.map((planet, index) => (
+        <h2 key={index}>{planet.structure}</h2>
+      ))}
+    </div>
+  );
 }
+
 export default Typesforplanet;
